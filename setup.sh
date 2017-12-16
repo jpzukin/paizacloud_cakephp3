bbfe5fa682fb0164b0a8752b47239d4daff995cd
jpnzukin <akai.tosaka@gmail.com>
2017-12-17 05:31:34 +0900

    Modify setup.sh
    intl PHP 拡張、zip PHP拡張のインストール
    Apache httpdのrewriteモジュールの有効化
    Apache httpdのServerNameの警告に対応
    CakePHP3用のデータベース作成と、ユーザーの追加
    ~/public_html以下にCakePHP3のプロジェクト作成

--- setup.sh
+++ setup.sh
@@ -1,5 +1,37 @@
 #!/bin/sh
 
-script_name=${basename $0}
+script_dir=$(dirname $(readlink $0))
 
-echo "START ${script_name}"
+cd ${script_dir}
+
+sudo sh << SCRIPT
+  sudo apt-get update
+  sudo apt-get upgrade -y
+
+  sudo apt-get install -y php7.0-intl php7.0-zip
+
+  echo "ServerName $HOSTNAME > /etc/apache2/conf-available/fqdn.conf"
+  a2enconf fqdn.conf
+  a2enmod rewrite
+  service apache2 restart
+SCRIPT
+
+if [ -f database.sql ]; then
+cat << SQL > database.sql 
+  tee result.log
+
+  GRANT ALL ON my_app.* TO my_app@localhost IDENTIFIED BY 'Pa$$w0rd';
+
+  CREATE DATABASE IF NOT EXISTS my_app DEFAULT CHARACTER SET utf8;
+
+  SHOW CREATE USER my_app@localhost;
+  SHOW GRANTS FOR my_app@localhost;
+  SHOW CREATE DATABASE my_app;
+
+  notee
+SQL
+mysql u root < database.sql
+fi
+
+cd ~/public_html
+conposer create-project --prefer-dist cakephp/app my_app_name --no-progress --profile
